045386e0eb32fe86e6b711ba271a8b53bbde92ea
Jaakko Huomo <jaakko.huomo@plugit.fi>
2023-09-15 12:18:26 +0300

    docs: more jsdoc documentation

--- src/index.ts
+++ src/index.ts
@@ -35,12 +35,75 @@ export default function FastifyMongooseRest<T>(
 ) {
   const newBasePath = addSlashToPath(basePath);
   return {
+    /**
+     * Route for creating a new resource
+     *
+     * @description
+     * Validates input with given {@link FastifyMongooseRestOptions.validationSchema validation schema} and returns the created resource
+     *
+     * @see {@link Model.create Mongooose create method}
+     */
     create: Create(newBasePath, model, options),
+
+    /**
+     * Route for deleting a resource by it's identifier
+     *
+     * @description
+     * Deletes a resource by _id or {@link FastifyMongooseRestOptions.findProperty findProperty option}
+     *
+     * @see {@link Model.deleteOne Mongooose deleteOne method}
+     */
     delete: Delete(newBasePath, model, options),
+
+    /**
+     * Route for displaying a single resource by it's identifier
+     *
+     * @description
+     * Returns a resource by _id or {@link FastifyMongooseRestOptions.findProperty findProperty option}
+     *
+     * @see {@link Model.findOne Mongooose findOne method}
+     */
     details: Details(newBasePath, model, options),
+
+    /**
+     * Route for displaying modifying a resource by it's identifier
+     *
+     * @description
+     * Validates input with given {@link FastifyMongooseRestOptions.validationSchema validation schema} and returns the modified resource
+     *
+     * @see {@link Model.findOne Mongooose findOne method}
+     * @see {@link Model.updateOne Mongooose updateOne method}
+     */
     modify: Modify(newBasePath, model, options),
+
+    /**
+     * Route for querying a list of resources
+     *
+     * @description
+     * Returns a list of resources defined by the query parameters used in the request
+     *
+     * @see {@link Model.find Mongooose find method}
+     */
     list: List(newBasePath, model, options),
+
+    /**
+     * Route for searching a list of resources
+     *
+     * @description
+     * Returns a list of resources defined by the body used in the request
+     *
+     * @see {@link Model.find Mongooose find method}
+     */
     search: Search(newBasePath, model, options),
+
+    /**
+     * Route for inserting many resources
+     *
+     * @description
+     * Validates input with given {@link FastifyMongooseRestOptions.validationSchema validation schema} and returns the created resources
+     *
+     * @see {@link Model.insertMany Mongooose insertMany method}
+     */
     insertMany: InsertMany(newBasePath, model, options),
   };
 }
--- src/types.ts
+++ src/types.ts
@@ -15,21 +15,67 @@ export interface FastifyMongooseRestOptions {
   findProperty?: string;
 }
 
+/**
+ * Find options for mongoose
+ */
 export interface FindOptions {
+  /**
+   * Query for mongoose
+   * @alias q
+   */
   query?: object | string;
+  /**
+   * Query for mongoose
+   * @alias query
+   */
   q?: object | string;
+  /**
+   * Populate for mongoose
+   */
   populate?: object | string;
+  /**
+   * Projection for mongoose
+   */
   projection?: object | string;
+  /**
+   * Sort for mongoose
+   */
   sort?: object | string;
+  /**
+   * Select for mongoose
+   */
   select?: object | string;
+  /**
+   * Skip for mongoose
+   */
   skip?: number;
+  /**
+   * Limit for mongoose
+   */
   limit?: number;
+  /**
+   * Page for mongoose
+   * @alias page
+   */
   p?: number;
+  /**
+   * Page for mongoose
+   * @alias p
+   */
   page?: number;
+  /**
+   * Page size for mongoose
+   */
   pageSize?: number;
+  /**
+   * Total count parameter for returning total count of documents of the query
+   */
   totalCount?: boolean;
 }
 
+/**
+ * Find options for mongoose
+ */
 export interface FindQueryOptions extends FindOptions {
   query?: string;
   q?: string;
